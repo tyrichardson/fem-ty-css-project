ea8cc51d647ba38823e87f217304416a0b9446b1
tyrichardson <32822802+tyrichardson@users.noreply.github.com>
2020-02-18 17:28:47 -0600

    testing touch up
    tested and touched up

--- script/script.js
+++ script/script.js
@@ -58,3 +58,26 @@ let mySwiper = new Swiper('.swiper-container', {
     el: '.swiper-scrollbar',
   },
 })
+
+const LAUNCH_DATE = document.querySelector('.launch-date-div')
+
+const ON_LAUNCH_DATE = document.querySelector('.happened-today');
+
+const HAPPENED_HIDDEN = document.querySelector('.happened');
+
+const HAPPENED_STORY = "https://byabbe.se/on-this-day/2/18/events.json";
+
+let happenedArray = [];
+
+async function getHappened() {
+  const res = await fetch(HAPPENED_STORY);
+  const resJson = await res.json();
+  happenedArray = resJson.events;
+  let val = Math.floor(Math.random() * 10);
+  ON_LAUNCH_DATE.innerText = `On this date in ${ happenedArray[val].year }, ${ happenedArray[val].description }`;
+  HAPPENED_HIDDEN.classList.remove('hidden');
+};
+
+LAUNCH_DATE.addEventListener("click", function () {
+  getHappened();
+});
